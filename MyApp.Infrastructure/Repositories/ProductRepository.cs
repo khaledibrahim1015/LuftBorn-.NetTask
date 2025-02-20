8adc053c750389ab536f6db103fb960a711577f4
khaledibrahim1015 <ibrahimkhaled136@gmail.com>
2025-02-20 13:09:45 +0200

    Repositories implemenattion

---
+++ MyApp.Infrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyApp.Core.Entitties;
+using MyApp.Core.Inrerfaces;
+using MyApp.Domain.Specifications;
+using MyApp.Infrastructure.Data;
+
+namespace MyApp.Infrastructure.Repositories;
+
+public class ProductRepository : RepositoryBase<Product, int>, IProductRepository
+{
+    public ProductRepository(AppDbContext context) : base(context)
+    {
+    }
+
+    public Task<IQueryable<Product>> GetProductByNameAsync(string name)
+    {
+        return Task.FromResult(_context.Products.Where(p => p.Name == name).AsQueryable());
+    }
+
+    public async Task<Pagination<Product>> GetProductPaginationAsync(PaginatedSpecsParams paginatedSpecsParams)
+    {
+        var query = GetWhereAsync(prd => prd.Name.ToLower().Contains(paginatedSpecsParams.Search.Trim().ToLower()));
+        var totalItems = await query.CountAsync();
+        var products = await query
+                        .OrderByDescending(x => x.Id)
+                        .Skip((paginatedSpecsParams.PageIndex - 1) * paginatedSpecsParams.PageSize)
+                        .Take(paginatedSpecsParams.PageSize)
+                        .ToListAsync();
+        return new Pagination<Product>
+        {
+            Data = products,
+            PageIndex = paginatedSpecsParams.PageIndex,
+            PageSize = paginatedSpecsParams.PageSize,
+            Count = totalItems
+        };
+    }
+}
---
+++ MyApp.Infrastructure/Repositories/RepositoryBase.cs
@@ -0,0 +1,56 @@
+﻿using MyApp.Core.Entitties;
+using MyApp.Core.Inrerfaces;
+using MyApp.Infrastructure.Data;
+using System.Linq.Expressions;
+
+namespace MyApp.Infrastructure.Repositories;
+
+public class RepositoryBase<TEntity, TKey> : IAsyncBaseRepository<TEntity, TKey>
+    where TEntity : BaseEntity<TKey>
+{
+    protected readonly AppDbContext _context;
+
+    public RepositoryBase(AppDbContext context)
+            => _context = context;
+
+
+    public async Task<IQueryable<TEntity>> GetAllAsync()
+       => await Task.FromResult(_context.Set<TEntity>().AsQueryable());
+
+    public async Task<IQueryable<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>> predicate)
+        => await Task.FromResult(_context.Set<TEntity>().Where(predicate).AsQueryable());
+
+    public async Task<TEntity> GetByIdAsync(TKey id)
+        => await _context.Set<TEntity>().FindAsync(id);
+
+    public async Task AddAsync(TEntity entity)
+    {
+        await _context.Set<TEntity>().AddAsync(entity);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(TEntity entity)
+    {
+        // Ensure the entity is being tracked
+        var existingEntity = await _context.Set<TEntity>().FindAsync(entity.Id);
+        if (existingEntity != null)
+        {
+            // Update the existing entity's properties with the values from the detached entity
+            _context.Entry(existingEntity).CurrentValues.SetValues(entity);
+            await _context.SaveChangesAsync();
+        }
+    }
+    public async Task DeleteAsync(TKey id)
+    {
+        var entity = await _context.Set<TEntity>().FindAsync(id);
+        if (entity != null)
+        {
+            _context.Set<TEntity>().Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+    }
+
+    public IQueryable<TEntity> GetWhereAsync(Expression<Func<TEntity, bool>> predicate)
+        => _context.Set<TEntity>().Where(predicate);
+
+}
