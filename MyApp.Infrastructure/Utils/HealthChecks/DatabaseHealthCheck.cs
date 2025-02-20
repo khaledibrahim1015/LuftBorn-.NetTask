a2de066e59fea2f5972475a3138bbd9d68daf143
khaledibrahim1015 <ibrahimkhaled136@gmail.com>
2025-02-20 13:10:02 +0200

    healthchecks utils

---
+++ MyApp.Infrastructure/Utils/HealthChecks/DatabaseHealthCheck.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace MyApp.Infrastructure.Utils.HealthChecks;
+
+/// <summary>
+/// Health check for the database connection.
+/// </summary>
+public class DatabaseHealthCheck : IHealthCheck
+{
+    private readonly string _connectionString;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DatabaseHealthCheck"/> class.
+    /// </summary>
+    /// <param name="configuration">The configuration to retrieve the connection string.</param>
+    /// <exception cref="ArgumentNullException">Thrown when the connection string is null or empty.</exception>
+    public DatabaseHealthCheck(IConfiguration configuration)
+    {
+        _connectionString = configuration.GetConnectionString("DatabaseSettings");
+        if (string.IsNullOrEmpty(_connectionString))
+            throw new ArgumentNullException(nameof(_connectionString), "ConnectionString string is missing ");
+    }
+
+    /// <summary>
+    /// Checks the health of the database connection.
+    /// </summary>
+    /// <param name="context">The context in which the health check is being run.</param>
+    /// <param name="cancellationToken">A token that can be used to cancel the health check.</param>
+    /// <returns>A <see cref="HealthCheckResult"/> indicating the health of the database connection.</returns>
+    public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+    {
+        using var sqlConnection = new SqlConnection(_connectionString);
+        await sqlConnection.OpenAsync(cancellationToken);
+        using var command = sqlConnection.CreateCommand();
+        command.CommandText = "SELECT 1";
+        try
+        {
+            await command.ExecuteNonQueryAsync(cancellationToken);
+            return HealthCheckResult.Healthy();
+        }
+        catch (Exception ex)
+        {
+            return HealthCheckResult.Unhealthy("Health check failed", ex);
+        }
+    }
+}
---
+++ MyApp.Infrastructure/Utils/HealthChecks/HealthCheckExtensions.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System.Net.Mime;
+using System.Text.Json;
+
+namespace MyApp.Infrastructure.Utils.HealthChecks;
+
+public class HealthCheckExtensions
+{
+
+    public static Task WriteHealthCheckResponse(HttpContext context, HealthReport result)
+    {
+        string json = JsonSerializer.Serialize(
+          new
+          {
+              Status = result.Status.ToString(),
+              Duration = result.TotalDuration,
+              Info = result.Entries.Select(pair => new
+              {
+                  pair.Key,
+                  pair.Value.Description,
+                  pair.Value.Duration,
+                  Status = Enum.GetName(typeof(HealthStatus), pair.Value.Status),
+                  Error = pair.Value.Exception?.Message,
+                  Data = pair.Value.Data.Select(p => new { p.Key, Value = p.Value.ToString() })
+              })
+          },
+          new JsonSerializerOptions
+          {
+              WriteIndented = true,
+              PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+              DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull
+          });
+        context.Response.ContentType = MediaTypeNames.Application.Json;
+        return context.Response.WriteAsync(json);
+    }
+
+}
