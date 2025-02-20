a94271400c6201f15be39497016638833c8fb405
khaledibrahim1015 <ibrahimkhaled136@gmail.com>
2025-02-20 13:07:52 +0200

    add specifications for core layer

---
+++ MyApp.Core/Specifications/PaginatedSpecsParams.cs
@@ -0,0 +1,34 @@
+﻿namespace MyApp.Domain.Specifications;
+
+public class PaginatedSpecsParams
+{
+    /// <summary>
+    /// pagination part 
+    /// </summary>
+
+    private int _pageIndex = 1;
+    public int PageIndex
+    {
+        get { return _pageIndex; }
+        set
+        {
+            if (value <= 0)
+                throw new ArgumentOutOfRangeException("page index start with 1 ");
+            else
+                _pageIndex = value;
+        }
+    }
+    private const int MaxPageSize = 70;
+    private int _pageSize = 10;
+    public int PageSize
+    {
+        get => _pageSize;
+        set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value;
+    }
+    /// <summary>
+    /// filter part 
+    /// </summary>
+    public string Search { get; set; } = string.Empty;
+
+
+}
